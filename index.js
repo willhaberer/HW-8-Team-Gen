1583643a8fc7fa3eda83a59304b04290939898c0
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-14 17:06:55 -0500

    created outline for functions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+function createTeamManager() {}
+
+function createEngineer() {}
+
+function createIntern() {}
+
+function init() {}
+
+init();
