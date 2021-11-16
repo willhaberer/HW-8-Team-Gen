1343d259fc668577bf44050f64f4b1245d1fbbe1
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-15 20:02:07 -0500

    created employee class

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-
+var role = "";
 function createTeamManager() {}
 
 function createEngineer() {}
 
 function createIntern() {}
 
-function init() {}
+function memberRole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the manager's name?",
+        name: "managername",
+      },
+      {
+        type: "input",
+        message: "What is the manager's employee ID?",
+        name: "managerid",
+      },
+    ])
+    .then((response) => {
+      console.log(response);
+      role = response.memberRole;
+    });
+}
+
+function init() {
+  memberRole();
+}
 
 init();
