f64bb542879d3d3e02a9ca4e05ebeb2bd7eb41bc
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-15 20:55:54 -0500

    generic questions for employee added

--- index.js
+++ index.js
@@ -49,6 +49,21 @@ function createIE() {
       name: "role",
       choices: ["Engineer", "Intern"],
     },
+    {
+      type: "input",
+      name: "id",
+      message: "What is the employee's id?",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "What is the employee's name?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is the employee's email?",
+    },
   ]);
 }
 
