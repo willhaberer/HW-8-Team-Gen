8975762dc347223ac39aa6903911cc1d0fdf2bb1
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-15 20:04:27 -0500

    created functions to return values, exported module

--- memberRoles/employee.js
+++ memberRoles/employee.js
@@ -4,4 +4,18 @@ class Employee {
     this.name = name;
     this.email = email;
   }
+
+  returnID() {
+    return this.id;
+  }
+
+  returnName() {
+    return this.name;
+  }
+
+  returnEmail() {
+    return this.email;
+  }
 }
+
+module.exports = Employee;
